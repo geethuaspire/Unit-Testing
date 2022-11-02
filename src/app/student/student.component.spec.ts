9eaf842b56f9bbae788398eacbcbf4cee1d3ebca
geethu_aspire <geethu.gopi@aspiresys.com>
2022-11-02 19:36:42 +0530

    unit test5

--- src/app/student/student.component.spec.ts
+++ src/app/student/student.component.spec.ts
@@ -1,11 +1,13 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { StudentComponent } from './student.component';
 
 describe('StudentComponent', () => {
   let component: StudentComponent;
   let fixture: ComponentFixture<StudentComponent>;
-
+  let deb:DebugElement;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ StudentComponent ]
@@ -15,9 +17,19 @@ describe('StudentComponent', () => {
     fixture = TestBed.createComponent(StudentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    deb = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Increase Count Click',()=>{
+    const h1 = deb.query(By.css('h1'));
+    const btn = deb.query(By.css('#btnincreaseNumber'));
+    btn.triggerEventHandler('click',{});
+    fixture.detectChanges();
+    expect(component.CountNumber).toEqual(parseInt (h1.nativeElement.innerText)); 
+  })
+
 });
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.scss']
 })
 export class StudentComponent implements OnInit {
-
+  CountNumber=0;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  saveDataIntoConsoe(info:any){
+    console.log(info);
+    
+  }
+   
+  increaseNumber(){
+    this.CountNumber = this.CountNumber + 1;
+  }
+  decreaseNumber(){
+    this.CountNumber = this.CountNumber - 1;
+  }
 }
