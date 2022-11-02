f6c92d93fa7bbf0c11a6cba729154baa8b1c8cea
geethu_aspire <geethu.gopi@aspiresys.com>
2022-11-02 19:30:55 +0530

    unit testing3

--- src/app/student/student.component.spec.ts
+++ src/app/student/student.component.spec.ts
@@ -1,20 +1,28 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { AppRoutingModule } from '../app-routing.module';
 
 import { StudentComponent } from './student.component';
+import { StudentService } from './student.service';
 
 describe('StudentComponent', () => {
   let component: StudentComponent;
   let fixture: ComponentFixture<StudentComponent>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
       TestBed.configureTestingModule({
       declarations: [ StudentComponent ],
-      providers:[],
-      imports:[AppRoutingModule]
+      providers:[StudentService,HttpClient],
+      imports:[AppRoutingModule,
+      HttpClientModule,
+      HttpClientTestingModule,
+    ]
     })
     .compileComponents();
-
     fixture = TestBed.createComponent(StudentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -23,4 +31,41 @@ describe('StudentComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("spyOn method",()=>{
+    spyOn(component,'calculate');
+    component.saveData();
+    expect(component.calculate).toHaveBeenCalled();
+
+  })
+
+  it("spyOn method  - 1",()=>{
+    spyOn(component,'calculate').and.returnValues(10,20);
+    let result = component.studentResult();
+    expect(result).toEqual("fail");
+
+  })
+
+  it("spyOn method  - 2",()=>{
+    spyOn(component,'calculate').and.returnValues(10,20);
+    let result = component.studentResult();
+    expect(result).toEqual("fail");
+
+  })
+
+  it("spyOn method  - 3",()=>{
+    let service = fixture.debugElement.injector.get(StudentService)
+    spyOn(service,"saveDetails").and.callFake(()=>{
+      return of({
+        "result1":200
+      })
+    });
+    component.saveData();
+    expect(component.result).toEqual({
+        "result1":200
+    })
+  })
+
+
+
 });
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,4 +1,6 @@
+import { ParseSourceFile } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from './student.service';
 
 @Component({
   selector: 'app-student',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  sum = 0;
+  result:any;
+  
+  constructor(public service:StudentService) { }
 
   ngOnInit(): void {
   }
+  calculate(num1:any , num2:any){
+    this.sum = num1 + num2;
+    return this.sum;
+  }
 
+  saveData(){
+    let info = {
+      sumVal:this.calculate(5,5),
+      name:"Dot Net Office"
+    };
+    this.saveDataInToConsol(info);
+    this.service.saveDetails(info).subscribe(res=>{
+      this.result = res;
+    })
+  }
+saveDataInToConsol(info:any){
+  console.log(info);
+  
+}
+studentResult(){
+  if(this.calculate(10,20) >= 40){
+    return "Pass";
+  }
+  else{
+    return "fail";
+  }
+}
 }
