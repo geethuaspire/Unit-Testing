16559ea150785392f7bafc2abaf1fa9774027380
geethu_aspire <geethu.gopi@aspiresys.com>
2022-11-07 11:44:37 +0530

    eventbinding in unit test

--- src/app/student/student.component.spec.ts
+++ src/app/student/student.component.spec.ts
@@ -20,4 +20,45 @@ describe('StudentComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('button1',()=>{
+    const element: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#button1');
+    expect(component.label).toEqual("DotNet");
+    element.click();
+    fixture.detectChanges();
+    expect(component.label).toEqual("DotNet office");
+  })
+
+  it('button2',()=>{
+    const element: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#button2');
+    expect(component.label).toEqual("DotNet");
+    element.click();
+    fixture.detectChanges();
+    expect(component.label).toEqual("label value change on button2");
+  })
+
+
+  it('textbox1',()=>{
+    const element: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#textbox1');
+    expect(component.label).toEqual("DotNet");
+    element.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(component.label).toEqual("onChangeinput label change");
+  })
+
+  it('textbox2',()=>{
+    const element: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#textbox2');
+    expect(component.label).toEqual("DotNet");
+    element.value = "Dotnet - Updated";
+    element.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(component.label).toEqual("Dotnet - Updated");
+  })
+
+
+
+
+
+
+
 });
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.scss']
 })
 export class StudentComponent implements OnInit {
-
+  label:string="DotNet";
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  buttonClick(){
+     this.label = "DotNet office";
+  }
+  buttonClick2(){
+     this.label ="label value change on button2"
+  }
+  onChangeInput(){
+      this.label ="onChangeinput label change";
+  }
+  onChangeLabelInput(event:any){
+      this.label = event.target.value;
+  }
 }
