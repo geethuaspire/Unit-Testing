b71dd408c46adf69cb72941dcaa98aad6ffe53a6
geethu_aspire <geethu.gopi@aspiresys.com>
2022-11-09 09:16:47 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComputeComponent } from './compute/compute.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComputeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
