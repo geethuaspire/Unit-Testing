651c1a00d31ae62df88cd327a1aba8492bb9218f
geethu_aspire <geethu.gopi@aspiresys.com>
2022-11-17 17:13:50 +0530

    initial commit

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
+})
+export class PostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getPosts();
+  }
+  getPosts(){
+
+  }
+  delete(post){
+    
+  }
+
+}
