f13cf89db0ba48d6df0542152b09760a60145e47
geethu_aspire <geethu.gopi@aspiresys.com>
2022-11-18 17:34:17 +0530

    unit testing

--- src/app/post/post.component.spec.ts
+++ src/app/post/post.component.spec.ts
@@ -1,14 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 import { PostComponent } from './post.component';
+import { PostService } from './post.service';
+import { mockPOSTS } from '../mock/post.mock';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('PostComponent', () => {
   let component: PostComponent;
   let fixture: ComponentFixture<PostComponent>;
-
+  //let POSTS: Post[];
+  let service:PostService;
+  let mockposts = mockPOSTS;
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
-      declarations: [ PostComponent ]
+      declarations: [
+         PostComponent 
+        ],
+      imports:[
+        HttpClientTestingModule,
+        HttpClientModule,
+      ],
+      providers:[
+        PostService,
+      ]
+      
+      
     })
     .compileComponents();
 
@@ -20,4 +38,47 @@ describe('PostComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should delete the selected post from the post',()=>{
+     //  service.deletePost.and.returnValue(of(true))
+       component.posts = mockPOSTS;
+       
+       component.delete(mockPOSTS[1]);
+       expect(component.posts.length).toBe(2);
+  });
+  // it('should call thedelete method in post service',fakeAsync(()=>{
+  //   let spy = spyOn(service,'deletePost').and.returnValue(of(mockposts))
+  //   component.delete(mockposts[1]);
+  //   tick();
+  //   expect(spy).toHaveBeenCalled();
+  // }));
+  
+
+
+  it('unit test for subscribe method',fakeAsync(()=>{
+    let spy = spyOn(service,'getPosts').and.returnValue(of(mockposts));
+    component.getPosts();
+    tick();
+    expect(spy).toHaveBeenCalled();
+  }));
+  
+
+  it('unit test for subscribe method',fakeAsync(()=>{
+    let spy = spyOn(service,'deletePost').and.returnValue(of(mockposts));
+    component.delete(mockposts[1]);
+    tick();
+    expect(spy).toHaveBeenCalled();
+  }));
+
+  it('should delete the actual selected posts in posts ',()=>{
+      //service.deletePost.and.returnValue(of(true));
+      component.posts = mockposts;
+      component.delete(mockposts[1]);
+      for(let post of mockposts){
+        expect(post).not.toEqual(mockposts[1]);
+      }
+  })
+  
+  
+
 });
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Post } from '../models/post';
+import { PostService } from './post.service';
 
 @Component({
   selector: 'app-post',
@@ -7,16 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  posts:Post[]=[];
+
+  constructor(private postservice:PostService) { }
 
   ngOnInit(): void {
     this.getPosts();
   }
   getPosts(){
-
+     this.postservice.getPosts().subscribe(posts=>{
+      this.posts = posts;
+     })
   }
-  delete(post){
-    
+  delete(post:Post){
+       this.posts = this.posts.filter(p => p.id != post.id);
+       this.postservice.deletePost(post).subscribe();
   }
 
 }
